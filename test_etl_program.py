77c64194376803c3c535015010f79b65aefadc85
Richard Ogoma <richard.ogoma@outlook.com>
2023-04-06 08:30:34 +0100

    Refactor testing code for performance

--- test_etl_program.py
+++ test_etl_program.py
@@ -1,24 +1,25 @@
 import petl as etl
-from configparser import ConfigParser
-import os
-import sys
+import random
 from etl_program import transform_data
 
-try:
-    config_file = "config.ini"
-    if not os.path.exists(config_file):
-        raise IOError(f"Configuration file {config_file} does not exist")
-    else:
-        config = ConfigParser()
-        config.read(config_file)
-        test_descriptor = config["FILE"]["test_file_desc"]
-except IOError as e:
-    print("Error: " + str(e))
-    sys.exit()
+random_row = random.randint(1, 6)
+# Read a random row from CSV file into a petl table
+table = (
+    etl.fromcsv("data/test_data.csv")
+    .addrownumbers()
+    .select("row", lambda v: v == random_row)
+    .cutout("row")
+)
+
+# sink random row to memory in bytes
+sink = etl.MemorySource()
+table.tocsv(sink)
+random_data = sink.getvalue()
 
 
 def test_transform_data():
     """There should be no record with missing values for test to pass"""
-    test_table = transform_data(test_descriptor)
+    source = etl.MemorySource(random_data)
+    test_table = transform_data(source)
     test_table2 = etl.select(test_table, lambda row: any(v is None for v in row))
     assert 0 == etl.nrows(test_table2)
